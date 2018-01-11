4ec037ddb2b618fb7011ccf3b25f2c1b4bbfc23a
a1p4ca <dreamaker7770@gmail.com>
2018-01-12 08:45:01 +0900

    :sparkles: Implement lcmSeq.

--- src/main/scala/snippets/Snippets.scala
+++ src/main/scala/snippets/Snippets.scala
@@ -7,24 +7,40 @@ object Snippets {
       * @return gcd of Seq of numbers
       */
     def gcdSeq(numbers: Seq[Int]): Int = {
-        /**
-          * Get gcd using pattern matching
-          *
-          * However, the simplest way to get gcd in Scala:
-          * {{{
-          *   if(b == 0) a else gcd(b, a % b)
-          * }}}
-          * But I just want to use pattern matching to show scala pattern matching.
-          * I prefer using pattern matching in this project if function can be solved with it.
-          *
-          * @param a
-          * @param b
-          * @return gcd of a and b
-          */
-        def gcd(a: Int, b: Int): Int = b match {
-            case 0 => a
-            case _ => gcd(b, a % b)
-        }
       numbers reduce { gcd }
     }
+
+    /**
+      * Calculates the lowest common multiple (lcm) of an array of numbers.
+      * @param numbers Seq of numbers
+      * @return lcm of Seq of numbers
+      */
+    def lcmSeq(numbers: Seq[Int]): Int = {
+        def lcm(a: Int, b: Int) = {
+            val g = gcd(a, b)
+            if (g == 0) 0 else a * b / g
+        }
+        numbers reduce { lcm }
+    }
+
+    /**
+      * Get gcd using pattern matching
+      *
+      * However, the simplest way to get gcd in Scala:
+      * {{{
+      *   if(b == 0) a else gcd(b, a % b)
+      * }}}
+      * But I just want to use pattern matching to show scala pattern matching.
+      * I prefer using pattern matching in this project if function can be solved with it.
+      *
+      * @param a
+      * @param b
+      * @return gcd of a and b
+      */
+    def gcd(a: Int, b: Int): Int = b match {
+        case 0 => a
+        case _ => gcd(b, a % b)
+    }
+
+
 }
--- src/test/scala/SnippetsSpec.scala
+++ src/test/scala/SnippetsSpec.scala
@@ -2,7 +2,7 @@ import org.scalatest._
 import snippets.Snippets
 
 class SnippetsSpec extends FlatSpec with Matchers {
-    "gcdSeq" should "returns the greatest common denominator of a seq of numbers " in {
+    "gcdSeq" should "returns the greatest common denominator of a Seq of numbers." in {
         val cases = List (
             Seq(0, 0, 1, 1, 1) -> 1,
             Seq(12, 12, 12, 12) -> 12,
@@ -11,6 +11,17 @@ class SnippetsSpec extends FlatSpec with Matchers {
         )
         cases foreach { t =>
             Snippets.gcdSeq(t._1) shouldEqual t._2
+        }
+    }
+    "lcdSeq" should "returns the lowest common multiple of a Seq of numbers."in {
+        val cases = List (
+            Seq(0, 0, 1, 1, 1) -> 0,
+            Seq(12, 12, 12, 12) -> 12,
+            Seq(18, 24, 36, 48, 60) -> 720,
+            Seq(128, 291, 491) -> 18288768
+        )
+        cases foreach { t =>
+            Snippets.lcmSeq(t._1) shouldEqual t._2
         }
     }
 }
