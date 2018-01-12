69c88c200d6f0423c7c5bccc414a0c1a11afbb2d
a1p4ca <dreamaker7770@gmail.com>
2018-01-12 09:52:42 +0900

    :recycle: Shorter is better.

--- src/main/scala/snippets/Snippets.scala
+++ src/main/scala/snippets/Snippets.scala
@@ -6,9 +6,7 @@ object Snippets {
       * @param numbers Seq of numbers
       * @return gcd of Seq of numbers
       */
-    def gcdSeq(numbers: Seq[Int]): Int = {
-      numbers reduce gcd
-    }
+    def gcdSeq(numbers: Seq[Int]): Int = numbers reduce gcd
 
     /**
       * Calculates the lowest common multiple (lcm) of an array of numbers.
@@ -23,24 +21,5 @@ object Snippets {
         numbers reduce lcm
     }
 
-    /**
-      * Get gcd using pattern matching
-      *
-      * However, the simplest way to get gcd in Scala:
-      * {{{
-      *   if(b == 0) a else gcd(b, a % b)
-      * }}}
-      * But I just want to use pattern matching to show scala pattern matching.
-      * I prefer using pattern matching in this project if function can be solved with it.
-      *
-      * @param a
-      * @param b
-      * @return gcd of a and b
-      */
-    def gcd(a: Int, b: Int): Int = b match {
-        case 0 => a
-        case _ => gcd(b, a % b)
-    }
-
-
+    def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
